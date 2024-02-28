2594dc0e4d25945a22f12a8258549584b1fe6bbf
unknown <seraj96088@gmail.com>
2024-02-29 00:29:07 +0530

    Adding some changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,23 +17,19 @@ public class Main {
             return x * y;
         }
     }
+
     public static void main(String[] args) {
         Product p = new Product();
 
         // Task 3: Call method to multiply two integers
         int result1 = p.product(2, 3);
-        //System.out.println("Product of 2 and 3 is: " + result1);
 
         // Task 4: Call method to multiply three integers
         int result2 = p.product(2, 3, 4);
-        //System.out.println("Product of 2, 3, and 4 is: " + result2);
 
         // Task 5: Call method to multiply two doubles
         double result3 = p.product(2.5, 3.5);
-        //System.out.println("Product of 2.5 and 3.5 is: " + result3);
-        System.out.println(result1+ " " + result2 + " " + result3);
 
-
+        System.out.println(result1 + " " + result2 + " " + result3);
     }
-
 }
