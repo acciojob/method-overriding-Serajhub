f1e34fd18e12a753f324c1c1e0acfbb4c76715fb
unknown <seraj96088@gmail.com>
2024-02-29 00:36:50 +0530

    Adding some changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,35 +1,27 @@
 package com.driver;
 
 public class Main {
-    public static class Product {
-        // Task 3: Create a method to multiply two integers
-        public int product(int x, int y) {
-            return x * y;
-        }
-
-        // Task 4: Create an overloaded method to multiply three integers
-        public int product(int x, int y, int z) {
-            return x * y * z;
-        }
-
-        // Task 5: Create an overloaded method to multiply two doubles
-        public double product(double x, double y) {
-            return x * y;
+    class A {
+        public String meth() {
+            return "Invoking method from class A";
         }
     }
 
-    public static void main(String[] args) {
-        Product p = new Product();
-
-        // Task 3: Call method to multiply two integers
-        int result1 = p.product(2, 3);
-
-        // Task 4: Call method to multiply three integers
-        int result2 = p.product(2, 3, 4);
-
-        // Task 5: Call method to multiply two doubles
-        double result3 = p.product(2.5, 3.5);
-
-        System.out.println(result1 + " " + result2 + " " + result3);
+    class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
     }
+
+    public class MethodOverridingExample {
+        public static void main(String[] args) {
+            A objA = new A();
+            System.out.println(objA.meth()); // Task 3
+
+            B objB = new B();
+            System.out.println(objB.meth()); // Task 5
+        }
+    }
+
 }
